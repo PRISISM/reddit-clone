a2926300eaf68fa9a84efc185b28922f55ed6025
toashel <shelchean@gmail.com>
2016-02-10 22:53:08 +1300

    Add user/show functionality and hyperlinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   devise_for :users
+  resources :users, only: [:show]
   resources :links do
     member do
       put "like", to: "links#upvote"
